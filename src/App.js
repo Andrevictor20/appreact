97a1782c5210422d6e994cc36868dc7c065dbd46
André Victor <victorandre280@gmail.com>
2024-01-19 02:17:46 -0300

    padrao

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
 import React from "react";
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: 2000,
-    cores: ["#29d8d5", "#252a34", "#fc3766"],
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: 3000,
-    cores: ["#ffd045", "#d4394b", "#f37c59"],
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: 1500,
-    cores: ["#365069", "#47c1c8", "#f95786"],
-  },
-];
 
 const App = () => {
-  const dados = produtos.filter(({ preco }) => preco > 1500);
-  return (
-    <section>
-      <h1>Produtos</h1>
-      {dados.map(({ id, nome, preco, cores }) => (
-        <div key={id}>
-          <h2>{nome}</h2>
-          <p>Preço: R${preco}</p>
-          <ul>
-          <p>#Cores Disponíveis:</p>
-            {cores.map((cor) => (
-              <li style={{ backgroundColor: cor, color: "white" }} key={cor}>
-                {cor}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
-  );
+  return <></>;
 };
 
 export default App;
