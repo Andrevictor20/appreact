e42d507d41da81ce3e6454dcc7f490eecf588ff0
André Victor <victorandre280@gmail.com>
2024-01-20 20:13:33 -0300

    useContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import Produto from './Produto';
+import { GlobalStorage } from './GlobalContext';
 
 const App = () => {
-  return <></>;
+  return (
+    <GlobalStorage>
+      <Produto />
+    </GlobalStorage>
+  );
 };
 
 export default App;
+
