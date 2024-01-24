060f7c11ec218189c85db15c535c167bc85e1cc3
André Victor <victorandre280@gmail.com>
2024-01-24 19:36:25 -0300

    uee form hook-ex de utilização

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Input from './Form/Input';
+import Input from './Forms/Input';
 import useValidateForm from './Hooks/useValidateForm';
 
 const App = () => {
   const cep = useValidateForm('cep');
+  const email= useValidateForm('email');
+  const nome = useValidateForm();
+  const sobrenome = useValidateForm(false);
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (cep.validate()) {
+    if (cep.validate() && email.validate()) {
       console.log('Enviar');
     } else {
       console.log('Não enviar');
@@ -17,11 +20,30 @@ const App = () => {
   return (
     <form onSubmit={handleSubmit}>
       <Input
+        label="Nome"
+        id="nome"
+        type="text"
+        {...nome}
+      />
+      <Input
+        label="Sobrenome"
+        id="sobrenome"
+        type="text"
+        {...sobrenome}
+      />
+      <Input
         label="CEP"
         id="cep"
         type="text"
         placeholder="00000-000"
         {...cep}
+      />
+      <Input
+        label="email"
+        id="email"
+        type="text"
+        placeholder="XXXXX@example.com"
+        {...email}
       />
       <button>Enviar</button>
     </form>
