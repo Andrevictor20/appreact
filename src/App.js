84449ba8017ec380ab2b3d8af89521854e9e2db5
André Victor <victorandre280@gmail.com>
2024-01-25 16:29:50 -0300

    styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const Titulo = styled.h1`
+  font-size: 2rem;
+  color: blue;
+`;
+
+const Paragrafo = styled.p`
+  font-size: 1.5rem;
+  color: red;
+`
 
 const App = () => {
-  return <></>;
+  return (
+    <div>
+      <Titulo>Meu App</Titulo>
+      <Paragrafo>Paragrafo</Paragrafo>
+    </div>
+  );
 };
 
 export default App;
