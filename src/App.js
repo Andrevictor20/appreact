f8e97b35d9de361af9931155cf5182caf0fcc0cc
André Victor <victorandre280@gmail.com>
2024-01-30 19:16:50 -0300

    part1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter,Routes,Route} from "react-router-dom";
+import Produtos from "./Components/Produtos";
 
 const App = () => {
   return (
   <>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Produtos/>} />
+
+    </Routes>
+  </BrowserRouter>
   </>
   );
 };
---
+++ src/Components/Produtos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Produtos.module.css'
+
+const Produtos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Produtos
